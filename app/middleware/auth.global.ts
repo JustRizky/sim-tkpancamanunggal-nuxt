99d10cc13d82cae53377f55ab0c40848fa3a1074
JustRizky <anandamrkl@gmail.com>
2025-11-20 22:46:32 +0700

    Revert "fix: fix middleware"
    This reverts commit c95bd833ffb16331ba36bbd31338d3d4b115333d.

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,24 +1,15 @@
 export default function authMiddleware(to: any, { navigateTo }: any) {
-  const token = useCookie('token')
+  const isClient = typeof window !== 'undefined'
 
-  if (token.value) {
-    try {
-      const payload = JSON.parse(Buffer.from(token.value.split('.')[1], 'base64').toString())
+  if (isClient) {
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token')
 
-      const now = Math.floor(Date.now() / 1000)
-      if (payload.exp && payload.exp < now) {
-        token.value = null
-      }
-    } catch (err) {
-      token.value = null
+    if (!token && to.path.startsWith('/dashboard/home')) {
+      return navigateTo('/login')
     }
-  }
 
-  if (!token.value && to.path.startsWith('/dashboard')) {
-    return navigateTo('/login')
-  }
-
-  if (token.value && to.path === '/login') {
-    return navigateTo('/dashboard/home')
+    if (token && to.path === '/login') {
+      return navigateTo('/dashboard/home')
+    }
   }
 }
