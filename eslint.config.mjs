ca64643927aeaaabeb91183e93d6a69e4fa266e4
JustRizky <anandamrkl@gmail.com>
2026-01-22 13:15:17 +0700

    test: fix ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
 
-export default withNuxt({
-  rules: {
-    'no-console': ['error'],
-    'vue/multi-word-component-names': 'off',
-    'vue/valid-template-root': 'off',
-    'vue/no-multiple-template-root': 'off',
+export default [
+  js.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.{js,ts,vue}'],
+    ignores: ['node_modules/**', '.nuxt/**', '.output/**', 'coverage/**'],
+    rules: {
+      'no-console': 'error',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off',
+    },
   },
-})
+]
