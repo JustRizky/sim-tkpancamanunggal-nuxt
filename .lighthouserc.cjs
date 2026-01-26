5f49f196ac82d6b55b3abae78018a9cffa9c4080
JustRizky <anandamrkl@gmail.com>
2026-01-27 00:53:22 +0700

    feat: add lighthouse performance testing

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -1,6 +1,23 @@
 module.exports = {
   ci: {
     assert: {
+      assertions: {
+        'bf-cache': ['warn'],
+        'color-contrast': ['warn'],
+        'document-title': ['warn'],
+        'errors-in-console': ['warn'],
+        'html-has-lang': ['warn'],
+        'image-delivery-insight': ['warn'],
+        'link-name': ['warn'],
+        'meta-description': ['warn'],
+        'network-dependency-tree-insight': ['warn'],
+        'unused-javascript': ['warn'],
+        'largest-contentful-paint': ['warn'],
+        'legacy-javascript': ['warn'],
+        'modern-image-formats': ['warn'],
+        'render-blocking-insight': ['warn'],
+        'render-blocking-resources': ['warn'],
+      },
       preset: 'lighthouse:recommended',
     },
     collect: {
